e7f16ce20bd9f27c6b7719566ed9ac35c3ae2782
neus <neus.rodriguez@wallapop.com>
2022-10-06 16:15:06 +0200

    don't collide with myself

--- src/main/kotlin/com/example/battlesnake/Logic.kt
+++ src/main/kotlin/com/example/battlesnake/Logic.kt
@@ -23,10 +23,12 @@ fun decideMove(request: MoveRequest): Direction {
 
         // TODO: Step 2 - Don't hit yourself.
         // Use information in the request to prevent your Battlesnake from colliding with itself.
-        // val myBody = request.you.body
+        val myBody = request.you.body
+        !myBody.contains(newPosition)
 
         // TODO: Step 3 - Don't collide with others.
         // Use information in the request to prevent your Battlesnake from colliding with others.
+
 
         // TODO: Step 4 - Find food.
         // Use information in the request to seek out and find food.
